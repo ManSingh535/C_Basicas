8e9382e7686ea29e286377bcbbb8a23ff33aa960
Man Singh <singhman30@gmail.com>
2023-05-23 21:42:46 +0530

    after mixed reset commited

--- cbasic/main.c
+++ cbasic/main.c
@@ -1,12 +1,23 @@
+/*
+	multiply all the digits of a number
+*/
+
 #include<stdio.h>
 #include<conio.h>
 
 int main()
 {
-	int num;
+	int num, digit_mul=1;
 	printf("Enter a number\n");
 	scanf("%d", &num);
 	printf("The entered number is: %d", num);
+	while(num>0)
+	{
+		digit_mul=digit_mul*(num%10);
+		num=num/10;
+
+	}
+  printf("Multiplying digit result: %d", digit_mul);
 	getch();
 	return 0;
 }
