ddc9a605111f56ad078f852aa5288f3463831787
Man Singh <singhman30@gmail.com>
2023-05-23 20:51:19 +0530

    sum of digits

--- cbasic/main.c
+++ cbasic/main.c
@@ -1,12 +1,23 @@
+/*
+	Find the sum of digits of the number entered.
+*/
+
+
 #include<stdio.h>
 #include<conio.h>
 
 int main()
 {
-	int num;
+	int num, digit_sum=0;
 	printf("Enter a number\n");
 	scanf("%d", &num);
 	printf("The entered number is: %d", num);
+	while(num>0)
+	{
+		digit_sum+=num%10;
+		num=num/10;
+	}
+printf("Sum of the digits is: %d", digit_sum);
 	getch();
 	return 0;
 }
