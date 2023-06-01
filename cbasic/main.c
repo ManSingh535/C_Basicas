765664d28b2ccf9db109797813a61888eda54361
Man Singh <singhman30@gmail.com>
2023-06-01 18:37:34 +0530

    triangle check feature added

--- cbasic/main.c
+++ cbasic/main.c
@@ -1,6 +1,7 @@
 /*
 
 	multiply all the digits of a number
+	check formation of triangle is possible or not.
 */
 
 
@@ -12,18 +13,26 @@ int main()
 {
 
 	
-	int num, digit_sum=0;
-
+	int num, digit_sum=0, a,b,c,triangle=0;
+	printf("Enter three sides of a triangle\n");
+	scanf("%d %d %d", &a, &b, &c);
 	printf("Enter a number\n");
 	scanf("%d", &num);
-	printf("The entered number is: %d", num);
+	printf("The entered number is: %d\n", num);
 	while(num>0)
 	{
 
 		digit_sum+=num%10;
 		num=num/10;
 	}
-printf("Sum of the digits is: %d", digit_sum);
+	
+	if((a<(b+c))&&(b<(a+c))&&(c<(a+b)))
+		triangle=1;
+	if(triangle)
+	printf("Triangle formation is possible\n");
+	else printf("With given dimensions triangle formation is not possible\n");
+	
+printf("Sum of the digits is: %d\n", digit_sum);
 
 	getch();
 	return 0;
